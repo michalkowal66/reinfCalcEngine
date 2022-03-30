2aad179b16febdd96e197abbf2938a0b066a29aa
Michal Kowal <michal.kowal.66@gmail.com>
2022-03-30 13:02:44 +0200

    Add calculations for beam in support section

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from json import load
-from math import ceil, pi
+from math import ceil, pi, sqrt
 from materialProperties.properties import properties
 
 class Element():
@@ -40,7 +40,53 @@ class Element():
         pass
 
     def calc_beam(self):
-        pass
+        # Get material properties
+        concrete_prop, steel_prop = self.get_material_properties()
+
+        # Concrete
+        f_ck = concrete_prop['fck']
+        f_cd = f_ck / 1.4
+
+        # Steel
+        f_yd = steel_prop['fyd']
+        bar_diam = float(self.el_data['b_bar_diam_combo']) / 10
+        stirrup_diam = 0.8
+
+        # Geometry
+        h = float(self.el_data['b_height_lineEdit'])
+        width = float(self.el_data['b_width_lineEdit'])
+        nom_cover = float(self.el_data['b_concr_cover_lineEdit'])
+
+        # Load
+        bend_moment = float(self.el_data['b_moment_lineEdit'])
+
+        # Effective width calculation
+        d = h - (nom_cover + stirrup_diam + 0.5 * bar_diam)
+
+        # check whether beam section is in support or span area
+        if self.el_data['b_sup_section_radioBtn']:
+            # Calculations for support section
+            mi = bend_moment / (width / 100 * ((d / 100) ** 2) * f_cd * 1000)
+
+            mi_lim = 0.374
+            if mi > mi_lim:
+                print("Error!")
+
+            alpha_1 = 0.973 - sqrt((0.974 - 1.95 * mi))
+
+            required_area = alpha_1 * width * d * (f_cd / f_yd)
+
+        else:
+            # Calculations for span section
+            pass
+
+        # Find provided area of reinforcement and amount of bars
+        provided_area, provided_bars = self.get_provided_reinforcement(required_area=required_area,
+                                                                       bar_diam=bar_diam,
+                                                                       stirrup_diam=stirrup_diam,
+                                                                       width=width,
+                                                                       cover=nom_cover)
+        return provided_area, provided_bars, required_area
 
     def calc_column(self):
         pass
