12f0f4db609254bed04d212237ecdba0344f073f
Michal Kowal <michal.kowal.66@gmail.com>
2022-03-30 12:49:20 +0200

    Create Element class structure and loader method

---
+++ src/main.py
@@ -0,0 +1,55 @@
+from json import load
+
+
+class Element():
+    def __init__(self, path):
+        self.path = path
+        file_dict = self.load_savefile(self.path)
+        self.el_type = file_dict['element'][:-4]
+        self.el_data = file_dict['data']
+        self.el_info = file_dict['info']
+
+    def load_savefile(self, path):
+        with open(path) as data_file:
+            data_dict = load(data_file)
+            return data_dict
+
+    def calc_reinforcement(self):
+        if self.validate_data(self.el_data):
+            if self.el_type == 'beam':
+                self.calc_beam()
+
+            elif self.el_type == 'col':
+                self.calc_column()
+
+            elif self.el_type == 'foot':
+                self.calc_foot()
+
+            elif self.el_type == 'plate':
+                self.calc_plate()
+
+    def get_material_properties(self):
+        pass
+
+    def validate_data(self, data_dict):
+        pass
+
+    def calc_beam(self):
+        pass
+
+    def calc_column(self):
+        pass
+
+    def calc_foot(self):
+        pass
+
+    def calc_plate(self):
+        pass
+
+    def get_provided_reinforcement(self, required_area, fi, fi_s, width, cover):
+        pass
+
+
+if __name__ == '__main__':
+    test_path = '../tests/beam_support_example.rcalc'
+    rc_element = Element(test_path)
